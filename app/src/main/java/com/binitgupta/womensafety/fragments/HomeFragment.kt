3ce1c7ba322f845863b2fb11d71b47ee4dbacd57
Binit Gupta <binitguptapro@gmail.com>
2022-11-13 06:19:52 +0530

    call-and-web-implemented

--- app/src/main/java/com/binitgupta/womensafety/fragments/HomeFragment.kt
+++ app/src/main/java/com/binitgupta/womensafety/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.binitgupta.womensafety.fragments
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import com.binitgupta.womensafety.BodyActivity
 import com.binitgupta.womensafety.HelplineNumberActivity
 import com.binitgupta.womensafety.R
+import kotlinx.android.synthetic.main.activity_helpline_number.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
@@ -30,6 +32,30 @@ class HomeFragment : Fragment() {
         helpline_call_btn.setOnClickListener{
             val intent = Intent(activity, HelplineNumberActivity::class.java)
             activity?.startActivity(intent)
+        }
+
+        call_police_fab.setOnClickListener {
+            val mNum = 100
+            val i = Intent()
+            i.action = Intent.ACTION_DIAL
+            i.data = Uri.parse("tel://$mNum")
+            activity?.startActivity(i)
+        }
+
+        useful_link_btn.setOnClickListener {
+            val address = "advocatechenoyceil.com/2018/01/06/women-safety-india-useful-links-numbers/"
+            val i = Intent()
+            i.action = Intent.ACTION_VIEW
+            i.data = Uri.parse("http://$address")
+            activity?.startActivity(i)
+        }
+
+        report_police_btn.setOnClickListener {
+            val address = "emailus.delhipolice.gov.in/DCPEmailid.aspx"
+            val i = Intent()
+            i.action = Intent.ACTION_VIEW
+            i.data = Uri.parse("http://$address")
+            activity?.startActivity(i)
         }
 
     }
