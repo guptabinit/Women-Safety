867208e1c6cece02c5355df0b12f330b0f8f34a9
Binit Gupta <binitguptapro@gmail.com>
2022-11-13 07:07:00 +0530

    gender-dropdown-implemented

--- app/src/main/java/com/binitgupta/womensafety/OneTimeInfoActivity.kt
+++ app/src/main/java/com/binitgupta/womensafety/OneTimeInfoActivity.kt
@@ -3,12 +3,17 @@ package com.binitgupta.womensafety
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_one_time_info.*
 
 class OneTimeInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one_time_info)
+
+        val genders = resources.getStringArray(R.array.gender)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, genders)
+        genderTextView.setAdapter(arrayAdapter)
 
         next_button.setOnClickListener{
             val intent = Intent(this, BodyActivity::class.java)
